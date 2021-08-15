77341d2f1761dac42d4baf08a08bc6adf68c1c87
Th0masL <thomas.lejeune@protonmail.com>
2021-08-15 14:21:59 +0300

    Updated scripts

--- veracrypt_mount.sh
+++ veracrypt_mount.sh
@@ -41,7 +41,7 @@ fi
 if [[ $( lsblk "$encrypted_partition" | grep "veracrypt" | grep "$mounting_point" ) ]]; then
 	msg "INFO - The encrypted partition $encrypted_partition is already mounted to $mounting_point"
 else
-	# Make sure that if the mounting point folder exists, it's empty (ignore the file 'unmounted' as we use it to know if the partition is mounted or not)
+	# Make sure that if the mounting point folder exists, it's empty
 	if [[ -d "$mounting_point" && $( ls "$mounting_point" | grep -v "unmounted" ) != "" ]]; then
 		msg "ERROR - The mounting point $mounting_point does not seems to be empty. Please verify the content of this folder."
 		exit 1
--- veracrypt_verify_partition_mounted.sh
+++ veracrypt_verify_partition_mounted.sh
@@ -13,16 +13,45 @@ script_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Get the script name
 script_name=$( basename "$0" )
 
+# Get the hostname of this machine
+server_name=$( hostname )
+
+# Function that returns the primary IP Address of this server
+function what_is_my_ip() {
+	# Use OpenDNS resolver to get my public IP
+	public=$( dig +short myip.opendns.com @resolver1.opendns.com | grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$" | head -1 )
+
+	# Do a basic 'ip route get' query against Google DNS to detect what is the default outgoing IP
+	default=$( ip route get 8.8.8.8 2>/dev/null | grep -E " src [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sed -e "s|.* src ||g" | awk '{print $1}' | head -1 )
+
+	# Return only what we want
+	if [[ "$1" == "public" ]]; then
+		echo "$public"
+	elif [[ "$1" == "default" ]]; then
+		echo "$default"
+	else
+		# If both IPs are th same, return only one
+		if [[ "$public" == "$default" ]]; then
+			echo "$default"
+		else
+			echo "$public - $default"
+		fi
+	fi
+}
+
 # Declare the function that print and log a msg
 function msg () {
-	echo "$1"
-	logger "$script_name - $1"
+	echo "$*"
+	logger "$script_name - $*"
 	# In case of error, try to send a Telegram message
-	if [[ $( echo "$1" | grep -i "^ERROR" ) && $send_telegram_alert -eq 1 ]]; then
+	if [[ $( echo "$1" | grep -E "^ERROR" ) && $send_telegram_alert -eq 1 ]]; then
 		# Check if the Telegram script exist
 		if [[ -f "/scripts/telegram_send_message.sh" && -x "/scripts/telegram_send_message.sh" ]]; then
+			# Get the server's IPs
+			server_all_ips=$( what_is_my_ip "all" )
+
 			# If the script exist, send the message
-			/scripts/telegram_send_message.sh --message "$script_name - $1"
+			/scripts/telegram_send_message.sh --message "$server_name ($server_all_ips) - $script_name - $*"
 		fi
 	fi
 }
@@ -41,7 +70,13 @@ if [[ "$encrypted_partition" == "" || "$mounting_point" == "" ]]; then
 	exit 1
 fi
 
-# Check if it's mounted or not
+# First, make sure that we can see the encrypted partition (for some reason, requires sudo when running in cronjob)
+if [[ -z $( sudo fdisk -l | grep "^/dev" | grep "$encrypted_partition" ) ]]; then
+	msg "ERROR - Unable to find the partition $encrypted_partition. If this parition is on an external disk, please verify that the disk is connected."
+	exit 1
+fi
+
+# If the partition exist, check if it's already mounted or not
 if [[ $( lsblk "$encrypted_partition" 2>/dev/null | grep "veracrypt" | grep "$mounting_point" ) ]]; then
 	msg "OK - The encrypted partition $encrypted_partition is currently mounted to the mounting point $mounting_point."
 else
